9391c3e3f9600dcaec1d1977f040a6ea241f1fb8
Ka Ming Chan <ming4883@gmail.com>
2016-03-19 14:04:28 +0800

    repo init

---
+++ Assets/MUD/Base/Editor/UI.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEditor;
+using System;
+
+namespace Mud
+{
+
+	public static class UI
+	{
+	    public static GUILayoutOption[] LAYOUT_DEFAULT = new GUILayoutOption[] { };
+	}
+
+}
